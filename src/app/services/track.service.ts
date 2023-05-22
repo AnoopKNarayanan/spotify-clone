06df2edd324e22d0506b61c3ee9a9137ccc845dc
AnoopKNarayanan <126202194+AnoopKNarayanan@users.noreply.github.com>
2023-05-22 12:21:52 -0600

    Update track.service.ts

--- src/app/services/track.service.ts
+++ src/app/services/track.service.ts
@@ -2,17 +2,16 @@ import { Injectable } from '@angular/core';
 import { Track } from '../models/track';
 import { HttpClient } from '@angular/common/http';
 import { Observable, map } from 'rxjs';
-import { ConfigService } from './config.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TrackService {
 
-  constructor(private http: HttpClient, private configService: ConfigService) { }
+  constructor(private http: HttpClient) { }
 
   getData(): Observable<any> {
-    const url = this.configService.getTracksUrl();
+    const url = 'https://api.npoint.io/3854b1336524d2b90eae';
     return this.http.get(url).pipe(
       map(resp => {
         return resp;
