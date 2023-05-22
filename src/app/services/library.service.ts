dba5d69df97554f801e49eb246f4c6be617d8e70
AnoopKNarayanan <126202194+AnoopKNarayanan@users.noreply.github.com>
2023-05-22 12:22:43 -0600

    Update library.service.ts

--- src/app/services/library.service.ts
+++ src/app/services/library.service.ts
@@ -2,17 +2,16 @@ import { Injectable } from '@angular/core';
 import { Artist, Library, SelectedSortOrder, SortOrder } from '../models/library';
 import { Observable, map } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-import { ConfigService } from './config.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LibraryService {
 
-  constructor(private http: HttpClient, private configService: ConfigService) { }
+  constructor(private http: HttpClient) { }
 
   getData(): Observable<any>{
-    const url = this.configService.getLibraryUrl();
+    const url = 'https://api.npoint.io/b73da9d9775da9a9d8cd';
     return this.http.get(url).pipe(
       map(resp => {
         return resp;
